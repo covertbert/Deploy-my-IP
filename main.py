1879aa061b84f9b9bc01a15cfc2f71e497026dc5
ROBERT G H BLACKMAN <robertghblackman@ROBERTs-MacBook-Pro.local>
2016-06-04 16:48:16 +0100

    Added comments and minor tweaks

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ def send_email():
     server.quit()
 
 
+# Checks the IP stored in ip.txt and returns it
 def check_stored_ip():
     with open('ip.txt', 'r') as data_file:
         data = data_file.read()
@@ -50,11 +51,14 @@ def check_stored_ip():
         return stripped_data
 
 
+# Writes the IP to ip.txt
 def write_ip_to_file():
     with open('ip.txt', 'w') as outfile:
         json.dump(get_ip(), outfile)
 
 
+# Compares the current IP with that from the txt file. If it hasn't changed it does nothing. If it has then it sends
+# the email and writes the new IP to the txt file
 def ip_compare():
     if check_stored_ip() != get_ip():
         print 'The IP has changed from ' + check_stored_ip() + ' to ' + get_ip() + ''
@@ -65,7 +69,10 @@ def ip_compare():
     else:
         print 'The IP hasn\'t changed'
 
+# Writes initial IP to text file when the script starts just in case the txt file doesn't already exist.
+write_ip_to_file()
 
+# Runs the ip_compare function every 5 minutes
 scheduler = BlockingScheduler()
 scheduler.add_job(ip_compare, 'interval', minutes=5)
 scheduler.start()
