ee597db1cdf953df5b992ae32bd43152536a0ecc
admin <admin@hl0045.creativelynx.ad>
2017-01-27 15:42:25 +0000

    Added firebase deploy of IP

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import socket
 import smtplib
 import json
+import subprocess
+import logging
 
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 from apscheduler.schedulers.blocking import BlockingScheduler
 
+logging.basicConfig()
 
 # Get password
 def get_password():
@@ -16,36 +17,18 @@ def get_password():
 
 # Get IP
 def get_ip():
-    ip_address = ([l for l in (
-        [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [
-            [(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in
-             [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
-    return ip_address
-
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
 
 # Send email with all the stuff
-def send_email():
-    fromaddr = "deployddetails@gmail.com"
-    toaddr = "blackmanrgh@gmail.com"
-    msg = MIMEMultipart()
-    msg['From'] = fromaddr
-    msg['To'] = toaddr
-    msg['Subject'] = "Your IP"
-
-    body = get_ip()
-    msg.attach(MIMEText(body, 'plain'))
-
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(fromaddr, get_password())
-    text = msg.as_string()
-    server.sendmail(fromaddr, toaddr, text)
-    server.quit()
-
+def deploy_ip():
+    bashCommand = 'firebase deploy'
+    subprocess.check_output(['bash','-c', bashCommand])
 
 # Checks the IP stored in ip.txt and returns it
 def check_stored_ip():
-    with open('ip.txt', 'r') as data_file:
+    with open('dist/ip.txt', 'r') as data_file:
         data = data_file.read()
         stripped_data = data[1:-1]
         return stripped_data
@@ -53,7 +36,7 @@ def check_stored_ip():
 
 # Writes the IP to ip.txt
 def write_ip_to_file():
-    with open('ip.txt', 'w') as outfile:
+    with open('dist/ip.txt', 'w') as outfile:
         json.dump(get_ip(), outfile)
 
 
@@ -62,11 +45,12 @@ def write_ip_to_file():
 def ip_compare():
     if check_stored_ip() != get_ip():
         print 'The IP has changed from ' + check_stored_ip() + ' to ' + get_ip()
-        send_email()
+        deploy_ip()
         print 'New IP as been sent to specified email'
         write_ip_to_file()
         print 'New IP has been written to text file for checking'
     else:
+        deploy_ip()
         print 'The IP hasn\'t changed'
 
 # Writes initial IP to text file when the script starts just in case the txt file doesn't already exist.
